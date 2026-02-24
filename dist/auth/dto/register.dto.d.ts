4ee023b0640d9322db81b1c47e06295bdb15d993
leormix <82319032+leormix@users.noreply.github.com>
2026-02-24 12:49:00 +0200

    Delete dist directory

--- dist/auth/dto/register.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class RegisterDto {
-    name: string;
-    surname: string;
-    email: string;
-    password: string;
-    roleId: number;
-}
--- dist/generated/prisma/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PaymentMethodType = void 0;
-exports.PaymentMethodType = {
-    CREDIT_CARD: 'CREDIT_CARD',
-    PAYPAL: 'PAYPAL',
-    BANK_TRANSFER: 'BANK_TRANSFER'
-};
-//# sourceMappingURL=enums.js.map
