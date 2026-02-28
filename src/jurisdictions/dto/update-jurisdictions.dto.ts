ab8718bcd2e1e3bc8480f6d7633fa506703c1abd
leormix <82319032+leormix@users.noreply.github.com>
2026-02-28 17:09:08 +0200

    Delete src directory

--- src/jurisdictions/dto/update-jurisdictions.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateJurisdictionDto } from './create-jurisdictions.dto';
-
-export class UpdateJurisdictionDto extends PartialType(CreateJurisdictionDto) { }
--- src/tax_rates/tax_rates.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { TaxRatesService } from './tax_rates.service';
-
-@Controller('tax-rates')
-export class TaxRatesController {
-  constructor(private readonly taxRatesService: TaxRatesService) {}
-}
