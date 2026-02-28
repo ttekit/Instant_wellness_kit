cbea25fa6de2293c2f5fbcd96751ca9c72c971e2
leormix <82319032+leormix@users.noreply.github.com>
2026-02-28 17:07:00 +0200

    Delete src directory

--- src/billings/dto/update-billing.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBillingDto } from './create-billing.dto';
-
-export class UpdateBillingDto extends PartialType(CreateBillingDto) {}
--- src/tax_rates/dto/update-tax-rate.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaxRateDto } from './create-tax-rate.dto';
-
-export class UpdateTaxRateDto extends PartialType(CreateTaxRateDto) { }
